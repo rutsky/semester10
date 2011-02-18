cf9954ea03e68fef596c110547713957a8926aea
Vladimir Rutsky <altsysrq@gmail.com>
2011-02-18 03:32:08 +0300

    Calculate frequencies for pairs

---
+++ Stochastic_models_of_computer_systems/lab1/table/frequency2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import locale
+import collections
+
+def main(args):
+    full_contents = sys.stdin.read()
+    #contents = filter(lambda x: len(x.encode("utf-8")) >= 2, full_contents)
+    contents = full_contents
+    counters = collections.defaultdict(int) # dict(character: count)
+    for ch in map(
+	    lambda x: ''.join(x), 
+	    zip(contents[:-1], contents[1:])):
+        counters[ch] += 1
+    
+    ch_entries = sorted(counters.items(), cmp=(lambda a, b: -cmp(a[1], b[1])))
+    total_count = len(contents)
+    for ch, count in ch_entries:
+        print("{0}\n{1:.16}".format(ch, float(count) / total_count))
+
+if __name__ == "__main__":
+    main(sys.argv)

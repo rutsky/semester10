90ac9c549da39ccb9fe6ae8626a8c4fbcd303d3f
Vladimir Rutsky <altsysrq@gmail.com>
2011-03-18 03:58:43 +0300

    Replace all trash with spaces and then replace all double spaces with single

--- Stochastic_models_of_computer_systems/lab1/table/generate.sh
+++ Stochastic_models_of_computer_systems/lab1/table/generate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cat ../data/war_and_peace.txt | sed 's/[^A-Za-z ]//g' | tr -d '\n' | tr 'A-Z' 'a-z' | \
+cat ../data/war_and_peace.txt | sed 's/[^A-Za-z ]/ /g' | tr '\n' ' ' | \
+    sed -r 's/[ ]+/ /g' | tr 'A-Z' 'a-z' > 1 | \
     python frequency.py > table.dat
 
-cat ../data/war_and_peace.txt | sed 's/[^A-Za-z ]//g' | tr -d '\n' | tr 'A-Z' 'a-z' | \
+cat ../data/war_and_peace.txt | sed 's/[^A-Za-z ]/ /g' | tr '\n' ' ' | \
+    sed -r 's/[ ]+/ /g' | tr 'A-Z' 'a-z' > 2 | \
     python frequency2.py > table2.dat
 
 # vim: set ts=2 sw=2 et:

7e8d2ccea8567aca41788d02814caebaeccaa714
Vladimir Rutsky <altsysrq@gmail.com>
2011-03-18 04:03:27 +0300

    Add generating tests from books

--- Stochastic_models_of_computer_systems/lab1/test/generate.sh
+++ Stochastic_models_of_computer_systems/lab1/test/generate.sh
@@ -4,4 +4,12 @@ i=1500
 
 python gen_random.py $i > random_$i.txt
 
+cat ../data/war_and_peace.txt | sed 's/[^A-Za-z ]/ /g' | tr '\n' ' ' | \
+    sed -r 's/[ ]+/ /g' | tr 'A-Z' 'a-z' | \
+    dd of=war_and_peace_$i.txt bs=1 count=$i skip=10000
+
+cat ../data/crime_and_punishment.txt | sed 's/[^A-Za-z ]/ /g' | tr '\n' ' ' | \
+    sed -r 's/[ ]+/ /g' | tr 'A-Z' 'a-z' | \
+    dd of=crime_and_punishment_$i.txt bs=1 count=$i skip=10000
+
 # vim: set ts=2 sw=2 et:

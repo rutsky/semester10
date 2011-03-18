0fbc004bb326cc20b0dee1198d497194297d5c92
Vladimir Rutsky <altsysrq@gmail.com>
2011-03-18 04:00:32 +0300

    Remove forgoten debug

--- Stochastic_models_of_computer_systems/lab1/table/generate.sh
+++ Stochastic_models_of_computer_systems/lab1/table/generate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cat ../data/war_and_peace.txt | sed 's/[^A-Za-z ]/ /g' | tr '\n' ' ' | \
-    sed -r 's/[ ]+/ /g' | tr 'A-Z' 'a-z' > 1 | \
+    sed -r 's/[ ]+/ /g' | tr 'A-Z' 'a-z' | \
     python frequency.py > table.dat
 
 cat ../data/war_and_peace.txt | sed 's/[^A-Za-z ]/ /g' | tr '\n' ' ' | \
-    sed -r 's/[ ]+/ /g' | tr 'A-Z' 'a-z' > 2 | \
+    sed -r 's/[ ]+/ /g' | tr 'A-Z' 'a-z' | \
     python frequency2.py > table2.dat
 
 # vim: set ts=2 sw=2 et:

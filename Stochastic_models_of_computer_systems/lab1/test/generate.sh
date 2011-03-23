afbc9a0d0144dc391f2b2b7423e677ebde90bfa9
Vladimir Rutsky <altsysrq@gmail.com>
2011-03-23 08:13:07 +0300

    Improve tests generation and running

--- Stochastic_models_of_computer_systems/lab1/test/generate.sh
+++ Stochastic_models_of_computer_systems/lab1/test/generate.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-i=1500
+i=2500
+offset=10000
 
-python gen_random.py $i > random_$i.txt
+for i in 1500 2500 4000 5000; do
+  python gen_random.py $i > random_$i.txt
 
-cat ../data/war_and_peace.txt | sed 's/[^A-Za-z ]/ /g' | tr '\n' ' ' | \
-    sed -r 's/[ ]+/ /g' | tr 'A-Z' 'a-z' | \
-    dd of=war_and_peace_$i.txt bs=1 count=$i skip=10000
-
-cat ../data/crime_and_punishment.txt | sed 's/[^A-Za-z ]/ /g' | tr '\n' ' ' | \
-    sed -r 's/[ ]+/ /g' | tr 'A-Z' 'a-z' | \
-    dd of=crime_and_punishment_$i.txt bs=1 count=$i skip=10000
+  for src in ../data/war_and_peace.txt ../data/crime_and_punishment.txt; do
+    cat $src | sed 's/[^A-Za-z ]/ /g' | tr '\n' ' ' | \
+        sed -r 's/[ ]+/ /g' | tr 'A-Z' 'a-z' | \
+        dd of=src_$src"_"$i.txt bs=1 count=$i skip=$offset
+  done
+done
 
 # vim: set ts=2 sw=2 et:
--- Stochastic_models_of_computer_systems/lab1/test/run.sh
+++ Stochastic_models_of_computer_systems/lab1/test/run.sh
@@ -4,13 +4,22 @@
 
 prg=../build/src/main
 
-args="../table/table.dat ../table/table2.dat 0.8 0.1 0.9"
+args="../table/table.dat ../table/table2.dat"
 
-for i in 1500; do
-  for test_name in war_and_peace random crime_and_punishment; do
-    cat $test_name"_"$i.txt | $prg $args > result_$test_name"_"$i.txt
-    mv frequency1.dat result_$test_name"_"frequency1_$i.dat
-    mv frequency2.dat result_$test_name"_"frequency2_$i.dat
+for i in 1500 2500 4000 5000; do
+  for gamma in 0.9 0.95 0.99; do
+    for alpha in 0.1; do
+      for zeta in 0.9; do
+        for test_name in war_and_peace random crime_and_punishment; do
+          suffix=_$test_name"_"$gamma"_"$alpha"_"$zeta"_"$i
+          cat $test_name"_"$i.txt | \
+              $prg $args $gamma $alpha $zeta > \
+              result$suffix.txt
+          mv frequency1.dat result_frequency1$suffix.dat
+          mv frequency2.dat result_frequency2$suffix.dat
+        done
+      done
+    done
   done
 done
 

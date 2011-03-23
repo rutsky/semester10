ebb00d8b06407eec3a93c30943c0214d55182842
Vladimir Rutsky <altsysrq@gmail.com>
2011-03-24 00:09:29 +0300

    Improve run script

--- Stochastic_models_of_computer_systems/lab1/test/run.sh
+++ Stochastic_models_of_computer_systems/lab1/test/run.sh
@@ -12,9 +12,10 @@ for i in 1500 2500 4000 5000 6000 7000; do
       for zeta in 0.900; do
         for test_name in war_and_peace random crime_and_punishment; do
           suffix=_$test_name$gamma"_"$alpha"_"$zeta"_"$i
-          cat src_$test_name"_"$i.txt | \
-              $prg $args $gamma $alpha $zeta > \
-              result$suffix.txt
+          echo Processing $test_name with $gamma $alpha $zeta $i...
+          echo cat src_$test_name"_"$i.txt \| $prg $args $gamma $alpha $zeta
+          (cat src_$test_name"_"$i.txt | \
+              $prg $args $gamma $alpha $zeta | tee result$suffix.txt) || exit 1
           mv frequency1.dat result$suffix"_frequency1.dat"
           mv frequency2.dat result$suffix"_frequency2.dat"
           mv bijections.dat result$suffix"_bijections.dat"

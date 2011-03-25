769d8360e205cbb49e437805364dd60e017f7f31
Vladimir Rutsky <altsysrq@gmail.com>
2011-03-25 08:07:58 +0300

    WIP

--- Stochastic_models_of_computer_systems/lab1/test/generate.sh
+++ Stochastic_models_of_computer_systems/lab1/test/generate.sh
@@ -3,7 +3,7 @@
 offset=10000
 tests_dir="../data/"
 
-for i in 500 1000 1500 2500 4000 5000 6000 7000 8000 9000 10000 12000 15000 20000; do
+for i in `seq 500 500 5000`; do
   python gen_random.py $i > src_random_$i.txt
 
   for test in war_and_peace crime_and_punishment; do
--- Stochastic_models_of_computer_systems/lab1/test/run.sh
+++ Stochastic_models_of_computer_systems/lab1/test/run.sh
@@ -7,22 +7,23 @@ prg=../build/src/release/main
 args="../table/table.dat ../table/table2.dat"
 
 #for i in 500 1000 1500 2500 4000 5000 6000 7000; do
-for i in 1500; do
+for i in 1000; do
   #for gamma in 0.900 0.950 0.990 0.999; do
   for gamma in 0.999; do
-    for alpha in 0.100; do
-      for zeta in 0.900; do
+    for alpha in 0.10; do
+      for alpha2 in 0.050; do
         for test_name in war_and_peace random crime_and_punishment; do
-          suffix=_$i"_"$test_name"_"$gamma"_"$alpha"_"$zeta
+          suffix=_$i"_"$test_name"_"$gamma"_"$alpha"_"$alpha2
 
           echo
           echo "****************"
-          echo "* Processing "$test_name with $gamma $alpha $zeta $i...
+          echo "* Processing "$test_name with $gamma $alpha $alpha2 $i...
           echo "****************"
-          echo cat src_$test_name"_"$i.txt \| $prg $args $gamma $alpha $zeta
+          echo cat src_$test_name"_"$i.txt \| $prg $args $gamma $alpha $alpha2 | \
+              tee result$suffix_note.txt
           failed=0
           (cat src_$test_name"_"$i.txt | \
-              $prg $args $gamma $alpha $zeta | \
+              $prg $args $gamma $alpha $alpha2 | \
                   stdbuf -o0 tee result$suffix.txt) || failed=1
           mv frequency1.dat result$suffix"_frequency1.dat"
           mv frequency2.dat result$suffix"_frequency2.dat"

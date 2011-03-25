926ddaf2bd90f7ea7bf3e35289ab30799232a7a6
Vladimir Rutsky <altsysrq@gmail.com>
2011-03-25 08:11:11 +0300

    More tests

--- Stochastic_models_of_computer_systems/lab1/test/run.sh
+++ Stochastic_models_of_computer_systems/lab1/test/run.sh
@@ -7,11 +7,11 @@ prg=../build/src/release/main
 args="../table/table.dat ../table/table2.dat"
 
 #for i in 500 1000 1500 2500 4000 5000 6000 7000; do
-for i in 1000; do
+for i in `seq 500 500 5000`; do
   #for gamma in 0.900 0.950 0.990 0.999; do
-  for gamma in 0.999; do
-    for alpha in 0.10; do
-      for alpha2 in 0.050; do
+  for gamma in 0.999 0.999999; do
+    for alpha in 0.010 0.050 0.100; do
+      for alpha2 in 0.010 0.050 0.100; do
         for test_name in war_and_peace random crime_and_punishment; do
           suffix=_$i"_"$test_name"_"$gamma"_"$alpha"_"$alpha2
 
@@ -20,7 +20,7 @@ for i in 1000; do
           echo "* Processing "$test_name with $gamma $alpha $alpha2 $i...
           echo "****************"
           echo cat src_$test_name"_"$i.txt \| $prg $args $gamma $alpha $alpha2 | \
-              tee result$suffix_note.txt
+              tee result$suffix"_note.txt"
           failed=0
           (cat src_$test_name"_"$i.txt | \
               $prg $args $gamma $alpha $alpha2 | \
@@ -29,6 +29,8 @@ for i in 1000; do
           mv frequency2.dat result$suffix"_frequency2.dat"
           test -e bijections.dat && \
               mv bijections.dat result$suffix"_bijections.dat"
+          test -e passed_bijections.dat && \
+              mv bijections.dat result$suffix"_passed_bijections.dat"
 
           if [ $failed == 1 ]; then
             exit 1;

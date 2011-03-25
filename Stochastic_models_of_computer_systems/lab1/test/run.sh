f9c9c0fdaae9f169e3c11cb86d2ffe796623d23a
Vladimir Rutsky <altsysrq@gmail.com>
2011-03-25 08:35:22 +0300

    WIP

--- Stochastic_models_of_computer_systems/lab1/test/run.sh
+++ Stochastic_models_of_computer_systems/lab1/test/run.sh
@@ -8,9 +8,10 @@ args="../table/table.dat ../table/table2.dat"
 
 #for i in 500 1000 1500 2500 4000 5000 6000 7000; do
 for i in `seq 500 500 5000`; do
-  #for gamma in 0.900 0.950 0.990 0.999; do
-  for gamma in 0.999 0.999999; do
-    for alpha in 0.010 0.050 0.100; do
+  #for gamma in 0.999 0.999999; do
+  for gamma in 0.999; do
+    #for alpha in 0.010 0.050 0.100; do
+    for alpha in 0.100; do
       for alpha2 in 0.010 0.050 0.100; do
         for test_name in war_and_peace random crime_and_punishment; do
           suffix=_$i"_"$test_name"_"$gamma"_"$alpha"_"$alpha2
@@ -30,7 +31,7 @@ for i in `seq 500 500 5000`; do
           test -e bijections.dat && \
               mv bijections.dat result$suffix"_bijections.dat"
           test -e passed_bijections.dat && \
-              mv bijections.dat result$suffix"_passed_bijections.dat"
+              mv passed_bijections.dat result$suffix"_passed_bijections.dat"
 
           if [ $failed == 1 ]; then
             exit 1;

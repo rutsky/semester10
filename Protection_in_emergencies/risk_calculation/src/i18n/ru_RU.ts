8dd8f7dae8d10191e18f755df273d1fca87f81ba
Vladimir Rutsky <altsysrq@gmail.com>
2011-05-01 21:15:24 +0400

    Add strings for translation

---
+++ Protection_in_emergencies/risk_calculation/src/i18n/ru_RU.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="forms/main_window.ui" line="14"/>
+        <source>Risk Calculation Suite</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="30"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="39"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="42"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

3fb74906efc16a92ae788781fc89484115823225
Vladimir Rutsky <altsysrq@gmail.com>
2011-05-01 21:23:22 +0400

    Translate and add compiled translation

--- Protection_in_emergencies/risk_calculation/src/i18n/ru_RU.ts
+++ Protection_in_emergencies/risk_calculation/src/i18n/ru_RU.ts
@@ -1,21 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="ru_RU">
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="forms/main_window.ui" line="14"/>
         <source>Risk Calculation Suite</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Вычисление рисков</translation>
     </message>
     <message>
         <location filename="forms/main_window.ui" line="30"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&amp;Файл</translation>
     </message>
     <message>
         <location filename="forms/main_window.ui" line="39"/>
         <source>&amp;Quit</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&amp;Выход</translation>
     </message>
     <message>
         <location filename="forms/main_window.ui" line="42"/>

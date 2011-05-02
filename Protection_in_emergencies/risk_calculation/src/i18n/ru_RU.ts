47e34c0ec0921b8b1a1117e49e09c1ee9aa8c0b1
Vladimir Rutsky <altsysrq@gmail.com>
2011-05-02 14:31:42 +0400

    Translate some items

--- Protection_in_emergencies/risk_calculation/src/i18n/ru_RU.ts
+++ Protection_in_emergencies/risk_calculation/src/i18n/ru_RU.ts
@@ -6,22 +6,161 @@
     <message>
         <location filename="forms/main_window.ui" line="14"/>
         <source>Risk Calculation Suite</source>
-        <translation type="unfinished">Вычисление рисков</translation>
+        <translation>Вычисление рисков</translation>
     </message>
     <message>
-        <location filename="forms/main_window.ui" line="30"/>
+        <location filename="forms/main_window.ui" line="348"/>
         <source>&amp;File</source>
-        <translation type="unfinished">&amp;Файл</translation>
+        <translation>&amp;Файл</translation>
     </message>
     <message>
-        <location filename="forms/main_window.ui" line="39"/>
+        <location filename="forms/main_window.ui" line="373"/>
         <source>&amp;Quit</source>
-        <translation type="unfinished">&amp;Выход</translation>
+        <translation>&amp;Выход</translation>
     </message>
     <message>
-        <location filename="forms/main_window.ui" line="42"/>
+        <location filename="forms/main_window.ui" line="376"/>
         <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="25"/>
+        <source>-lg &lt;i&gt;q&lt;sub&gt;real, 0&lt;/sub&gt;&lt;/i&gt; =</source>
+        <translation>-lg &lt;i&gt;q&lt;sub&gt;реальн, 0&lt;/sub&gt;&lt;/i&gt; =</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="48"/>
+        <source>-lg &lt;i&gt;q&lt;sub&gt;real, 1&lt;/sub&gt;&lt;/i&gt; =</source>
+        <translation>-lg &lt;i&gt;q&lt;sub&gt;реальн, 1&lt;/sub&gt;&lt;/i&gt; =</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="75"/>
+        <source>lg |&lt;i&gt;R&lt;sub&gt;0&lt;/sub&gt;&lt;/i&gt;| =</source>
+        <translation>lg |&lt;i&gt;R&lt;sub&gt;0&lt;/sub&gt;&lt;/i&gt;| =</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="111"/>
+        <source>-lg &lt;i&gt;q&lt;sub&gt;project, 0&lt;/sub&gt;&lt;/i&gt; =</source>
+        <translation>-lg &lt;i&gt;q&lt;sub&gt;проектн, 0&lt;/sub&gt;&lt;/i&gt; =</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="137"/>
+        <source>-lg &lt;i&gt;step&lt;/i&gt; =</source>
+        <translation>-lg &lt;i&gt;шаг&lt;/i&gt; =</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="160"/>
+        <source>-lg &lt;i&gt;q&lt;sub&gt;project, 1&lt;/sub&gt;&lt;/i&gt; =</source>
+        <translation>-lg &lt;i&gt;q&lt;sub&gt;проектн, 1&lt;/sub&gt;&lt;/i&gt; =</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="187"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;lg |&lt;span style=&quot; font-style:italic;&quot;&gt;R&lt;/span&gt;&lt;span style=&quot; font-style:italic; vertical-align:sub;&quot;&gt;1&lt;/span&gt;| =&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="238"/>
+        <source>&lt;i&gt;h&lt;/i&gt; =</source>
+        <translation>&lt;i&gt;h&lt;/i&gt; =</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="268"/>
+        <source>&amp;delta; =</source>
+        <translation>&amp;delta; =</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="284"/>
+        <source>&amp;deg;</source>
+        <translation>&amp;deg;</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="311"/>
+        <source>&lt;i&gt;S&lt;/i&gt; =</source>
+        <translation>&lt;i&gt;S&lt;/i&gt; =</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="363"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Правка</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="381"/>
+        <source>&amp;Save Parameters</source>
+        <translation>&amp;Сохранить параметры</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="384"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="389"/>
+        <source>S&amp;ave Parameters As...</source>
+        <translation>Сохранить параметры &amp;как...</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="392"/>
+        <source>Ctrl+Shift+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="397"/>
+        <source>&amp;Load Parameters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="402"/>
+        <source>L&amp;oad Parameters...</source>
+        <translation>&amp;Загрузить параметры...</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="405"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="410"/>
+        <source>Save &amp;Image As...</source>
+        <translation>Cохранить &amp;изображение как...</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="413"/>
+        <source>Ctrl+I</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="418"/>
+        <source>&amp;Print</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="421"/>
+        <source>Ctrl+P</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="426"/>
+        <source>&amp;New Parameters</source>
+        <translation>&amp;Новые параметры</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="429"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="434"/>
+        <source>&amp;Clear Marks</source>
+        <translation>&amp;Очистить метки</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="437"/>
+        <source>Del</source>
+        <translation></translation>
     </message>
 </context>
 </TS>

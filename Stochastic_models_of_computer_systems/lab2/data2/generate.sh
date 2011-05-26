9aa96f57f2fc1a4329033f92c2577678a150ebbd
Vladimir Rutsky <altsysrq@gmail.com>
2011-05-26 20:22:13 +0400

    Tune generation parameters

--- Stochastic_models_of_computer_systems/lab2/data2/generate.sh
+++ Stochastic_models_of_computer_systems/lab2/data2/generate.sh
@@ -3,11 +3,11 @@
 for N in 5000; do
   for dt in 1; do
     for m in 0; do
-      for sigma in 1; do
-        for lambda in 200; do
-          for m_signal in 20; do
+      for sigma in 0.3; do
+        for lambda in 500; do
+          for m_signal in 50; do
             for sigma_signal2 in 1; do
-              for lambda_signal in 0.05; do
+              for lambda_signal in 0.1; do
                 ./generate.py $N $dt $m $sigma $lambda $m_signal \
                     $sigma_signal2 $lambda_signal
               done

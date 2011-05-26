f21290b75ecbbd4846986c51e3180d13ccb67d62
Vladimir Rutsky <altsysrq@gmail.com>
2011-05-27 01:01:12 +0400

    Tune parameters. Additional debug ouput

--- Stochastic_models_of_computer_systems/lab2/data/do.sh
+++ Stochastic_models_of_computer_systems/lab2/data/do.sh
@@ -12,11 +12,8 @@ function process()
       for request_det_alpha in 0.001; do
         for n_hist_intervals in 50; do
           for max_em_it in 100; do
-            for max_em_delta in 0.01; do
-              run_suffix=_quiet-period="$quiet_period"_req-det-alpha="$request_det_alpha"_hist-ints="$n_hist_intervals"_max-em-it="$max_em_it"_max-em-delta="$max_em_delta"
-              echo
-              echo "**********"
-              echo "Generated: "$gen_suffix
+            for max_em_delta in 0.0001; do
+              run_suffix=_q-p="$quiet_period"_req-det-a="$request_det_alpha"_hist-ints="$n_hist_intervals"_max-em-it="$max_em_it"_max-em-d="$max_em_delta"
               echo "Run: --load-file-name=$load_file --quiet-period=$quiet_period --requests-detection-alpha=$request_det_alpha --histogram-intervals=$n_hist_intervals --max-em-iterations=$max_em_it --max-em-delta=$max_em_delta"
               ../build/src/release/main $load_file $dt $quiet_period \
                   $request_det_alpha $n_hist_intervals $max_em_it \
@@ -39,14 +36,18 @@ function process()
   done
 }
 
-for N in 1000 10000; do
+for N in 2000; do
   for dt in 1; do
     for m in 0; do
       for sigma in 1; do
         for lambda in 50; do
-          for m_signal in 10; do
+          for m_signal in 50; do
             for sigma_signal in 1; do
-              gen_suffix=_N="$N"_dt="$dt"_m="$m"_sigma="$sigma"_lambda="$lambda"_m_c="$m_signal"_sigma_c="$sigma_signal"
+              gen_suffix=_N="$N"_dt="$dt"_m="$m"_s="$sigma"_l="$lambda"_m_c="$m_signal"_s_c="$sigma_signal"
+
+              echo "**********"
+              echo "Generating: "$gen_suffix
+              echo
 
               ./generate.py $N $dt $m $sigma $lambda $m_signal $sigma_signal
               ./draw_graphs.sh
@@ -61,6 +62,8 @@ for N in 1000 10000; do
                     true_histogram.pdf; do
                 mv $f result"$gen_suffix"_$f
               done
+
+              echo
             done
           done
         done

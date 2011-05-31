169ab15cce4a88796b9890fa6fab985b01c929d1
Vladimir Rutsky <altsysrq@gmail.com>
2011-05-31 06:42:27 +0400

    Obtain K/sigma stat

--- Stochastic_models_of_computer_systems/lab2/data/do.sh
+++ Stochastic_models_of_computer_systems/lab2/data/do.sh
@@ -2,10 +2,14 @@
 
 #set -x
 
+stat="stat.csv"
+rm $stat
+
 function process()
 {
   gen_prefix=$1
   load_file="total_load.csv"
+  sigma=$3
 
   for dt in 1; do
     for quiet_period in 10; do
@@ -15,11 +19,14 @@ function process()
             for max_em_delta in 0.0001; do
               run_suffix=_q-p="$quiet_period"_req-det-a="$request_det_alpha"_hist-ints="$n_hist_intervals"_max-em-it="$max_em_it"_max-em-d="$max_em_delta"
               echo "Run: --load-file-name=$load_file --quiet-period=$quiet_period --requests-detection-alpha=$request_det_alpha --histogram-intervals=$n_hist_intervals --max-em-iterations=$max_em_it --max-em-delta=$max_em_delta"
+
+              output=result$gen_suffix$run_suffix"_output.txt"
+
               ../build/src/release/main $load_file $dt $quiet_period \
                   $request_det_alpha $n_hist_intervals $max_em_it \
-                  $max_em_delta | tee result$gen_suffix$run_suffix"_output.txt"
+                  $max_em_delta | tee $output
 
-              ./draw_detection.sh
+              #./draw_detection.sh
 
               for f in detected_iterative_requests.txt \
                     detected_iterative_requests.pdf \
@@ -28,6 +35,10 @@ function process()
                     histogram.csv histogram.pdf; do
                 mv $f result$gen_suffix$run_suffix"_"$f
               done
+
+              echo -n $sigma"," >> $stat
+              grep -e 'Detected [0-9]* requests' -o $output | tr -d 'A-Za-z ' | tr '\n' ',' | sed -r 's/,$//' >> $stat
+              echo >> $stat
             done
           done
         done
@@ -36,12 +47,13 @@ function process()
   done
 }
 
-for N in 2000; do
+for N in 10000; do
   for dt in 1; do
     for m in 0; do
-      for sigma in 1; do
+      for sigma in `seq 1.3 0.01 1.45`; do
+        sigma=`echo $sigma | tr ',' '.'`
         for lambda in 50; do
-          for m_signal in 4 6 8 10; do
+          for m_signal in 10; do
             for sigma_signal in 1; do
               gen_suffix=_N="$N"_dt="$dt"_m="$m"_s="$sigma"_l="$lambda"_m_c="$m_signal"_s_c="$sigma_signal"
 
@@ -50,9 +62,9 @@ for N in 2000; do
               echo
 
               ./generate.py $N $dt $m $sigma $lambda $m_signal $sigma_signal
-              ./draw_graphs.sh
+              #./draw_graphs.sh
 
-              process "$gen_prefix" total_load.csv
+              process "$gen_prefix" total_load.csv $sigma
 
               for f in requests.csv requests.pdf \
                     requests_num.csv requests_num.pdf \

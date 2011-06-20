3730f9e96685c92badc2e21f49be64e096ea098e
Vladimir Rutsky <altsysrq@gmail.com>
2011-06-21 02:53:36 +0400

    Add empty main application

--- Internet_technologies/unknown_words/server/settings.py
+++ Internet_technologies/unknown_words/server/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = (
     'djangotoolbox',
     'autoload',
     'dbindexer',
+    'learnsubtitles',
 
     # djangoappengine should come last, so it can override a few manage.py commands
     'djangoappengine',
